ae0b94af26d2235e0329309926fcb608e98fcde5
arthurd3 <arthur_camposl@yahoo.com>
2025-07-12 11:09:20 -0300

    feat: creating a scheduling class / scheduling enum

--- scheduling-api/src/main/java/com/arthur/schedulingApi/controllers/service/ServiceController.java
+++ scheduling-api/src/main/java/com/arthur/schedulingApi/controllers/service/ServiceController.java
@@ -2,6 +2,7 @@ package com.arthur.schedulingApi.controllers.service;
 
 import com.arthur.schedulingApi.controllers.service.request.ServiceRequestDTO;
 import com.arthur.schedulingApi.controllers.service.response.ServiceResponseDTO;
+import com.arthur.schedulingApi.usecases.service.CreateService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,9 +12,16 @@ import java.util.Optional;
 @RequestMapping("/api/v1/services")
 public class ServiceController {
 
-    @PostMapping("/create/{id}")
-    public ResponseEntity<Optional<ServiceResponseDTO>> createService(@PathVariable String ownerId , @RequestBody ServiceRequestDTO serviceRequestDTO){
-        return ResponseEntity.ok(Optional.of(null));
+    private final CreateService createService;
+
+    public ServiceController(CreateService createService) {
+        this.createService = createService;
+    }
+
+
+    @PostMapping("/create/{ownerId}")
+    public ResponseEntity<Optional<ServiceResponseDTO>> createService(@PathVariable(name = "ownerId") Long ownerId , @RequestBody ServiceRequestDTO serviceRequestDTO){
+        return ResponseEntity.ok(createService.createService(ownerId,serviceRequestDTO));
     }
 
 
---
+++ scheduling-api/src/main/java/com/arthur/schedulingApi/models/scheduling/SchedulingStatus.java
@@ -0,0 +1,8 @@
+package com.arthur.schedulingApi.models.scheduling;
+
+public enum SchedulingStatus {
+    AVAILABLE,
+    BOOKED,
+    CANCELED,
+    COMPLETED
+}
