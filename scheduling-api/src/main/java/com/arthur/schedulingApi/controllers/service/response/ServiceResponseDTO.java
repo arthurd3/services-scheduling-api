af65b00d9a12179b9b49bc9bfcda4f2dbe61f43e
arthurd3 <arthur_camposl@yahoo.com>
2025-07-12 10:47:34 -0300

    feat: create a service and dtos for service

--- scheduling-api/src/main/java/com/arthur/schedulingApi/controllers/service/response/ServiceResponseDTO.java
+++ scheduling-api/src/main/java/com/arthur/schedulingApi/controllers/service/response/ServiceResponseDTO.java
@@ -1,4 +1,16 @@
 package com.arthur.schedulingApi.controllers.service.response;
 
-public class ServiceResponseDTO {
+import com.arthur.schedulingApi.models.service.Scheduling;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public record ServiceResponseDTO (String name,
+                                  Long ownerId,
+                                  Integer capacity,
+                                  String description,
+                                  String location,
+                                  String url_image,
+                                  List<Scheduling>scheduling,
+                                  LocalDateTime createdAt) {
 }
--- scheduling-api/src/main/java/com/arthur/schedulingApi/usecases/service/mapper/ServiceToModel.java
+++ scheduling-api/src/main/java/com/arthur/schedulingApi/usecases/service/mapper/ServiceToModel.java
@@ -1,4 +1,24 @@
 package com.arthur.schedulingApi.usecases.service.mapper;
 
+import com.arthur.schedulingApi.controllers.service.request.ServiceRequestDTO;
+import com.arthur.schedulingApi.models.service.Service;
+
+import java.util.ArrayList;
+
 public class ServiceToModel {
+
+    public static Service serviceToModel(ServiceRequestDTO serviceRequestDTO) {
+
+        return new  Service(
+                serviceRequestDTO.id(),
+                serviceRequestDTO.name(),
+                serviceRequestDTO.capacity(),
+                serviceRequestDTO.description(),
+                serviceRequestDTO.location(),
+                serviceRequestDTO.url_image(),
+                new ArrayList<>()
+        );
+
+    }
+
 }
