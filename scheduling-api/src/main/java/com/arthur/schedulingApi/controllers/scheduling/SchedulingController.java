f00e0b1ef8a49ec3b6b179202e7348dc913321c2
arthurd3 <arthur_camposl@yahoo.com>
2025-07-14 14:56:06 -0300

    feat: delete scheduling

--- scheduling-api/src/main/java/com/arthur/schedulingApi/controllers/scheduling/SchedulingController.java
+++ scheduling-api/src/main/java/com/arthur/schedulingApi/controllers/scheduling/SchedulingController.java
@@ -1,8 +1,10 @@
 package com.arthur.schedulingApi.controllers.scheduling;
 
+import com.arthur.schedulingApi.controllers.ApiResponseDTO;
 import com.arthur.schedulingApi.controllers.scheduling.request.SchedulingRequestDTO;
 import com.arthur.schedulingApi.controllers.scheduling.response.SchedulingResponseDTO;
 import com.arthur.schedulingApi.usecases.scheduling.CreateScheduling;
+import com.arthur.schedulingApi.usecases.scheduling.DeleteScheduling;
 import com.arthur.schedulingApi.usecases.scheduling.FindScheduling;
 import com.arthur.schedulingApi.usecases.scheduling.FindUserScheduling;
 import org.springframework.data.domain.PageRequest;
@@ -19,11 +21,13 @@ public class SchedulingController {
     private final CreateScheduling createScheduling;
     private final FindScheduling findScheduling;
     private final FindUserScheduling findUserScheduling;
+    private final DeleteScheduling deleteScheduling;
 
-    public SchedulingController(CreateScheduling createScheduling, FindScheduling findScheduling, FindUserScheduling findUserScheduling) {
+    public SchedulingController(CreateScheduling createScheduling, FindScheduling findScheduling, FindUserScheduling findUserScheduling, DeleteScheduling deleteScheduling) {
         this.createScheduling = createScheduling;
         this.findScheduling = findScheduling;
         this.findUserScheduling = findUserScheduling;
+        this.deleteScheduling = deleteScheduling;
     }
 
     @PostMapping("/create")
@@ -36,10 +40,16 @@ public class SchedulingController {
         return ResponseEntity.ok(Optional.of(findScheduling.findScheduling(id)));
     }
 
-    @GetMapping("/findById/{id}")
-    public ResponseEntity<List<SchedulingResponseDTO>> findUserSchedulings(@PathVariable(name = "id") Long userId ,
+    @GetMapping("/findUserScheduling/{id}")
+    public ResponseEntity<List<SchedulingResponseDTO>> findUserScheduling(@PathVariable(name = "id") Long userId ,
                                                                            @RequestParam(value = "page", defaultValue = "0") int page,
                                                                            @RequestParam(value = "size", defaultValue = "5") int size) {
         return ResponseEntity.ok(findUserScheduling.findUserScheduling(userId , PageRequest.of(page , size)));
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<ApiResponseDTO> deleteScheduling(@PathVariable(name = "id") Long idScheduling) {
+        deleteScheduling.deleteScheduling(idScheduling);
+        return ResponseEntity.ok(new ApiResponseDTO("Agendamento deletado com sucesso!"));
     }
 }
--- scheduling-api/src/main/java/com/arthur/schedulingApi/usecases/scheduling/DeleteScheduling.java
+++ scheduling-api/src/main/java/com/arthur/schedulingApi/usecases/scheduling/DeleteScheduling.java
@@ -1,4 +1,21 @@
 package com.arthur.schedulingApi.usecases.scheduling;
 
+import com.arthur.schedulingApi.exceptions.SchedulingNotFoundException;
+import com.arthur.schedulingApi.models.scheduling.Scheduling;
+import com.arthur.schedulingApi.repositories.scheduling.SchedulingRepository;
+
 public class DeleteScheduling {
+
+    private final SchedulingRepository schedulingRepository;
+
+    public DeleteScheduling(SchedulingRepository schedulingRepository) {
+        this.schedulingRepository = schedulingRepository;
+    }
+
+    public void deleteScheduling(Long idScheduling) {
+        if(!schedulingRepository.existsById(idScheduling)) {
+            throw new SchedulingNotFoundException("Nao foi Possivel deletar o agendamento com id " + idScheduling + "nao existe!");
+        }
+        schedulingRepository.deleteById(idScheduling);
+    }
 }
