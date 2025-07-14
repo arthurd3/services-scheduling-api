a54d33119c2ae9fb67151f12f84f00a3d613de64
arthurd3 <arthur_camposl@yahoo.com>
2025-07-13 22:56:47 -0300

    feat: scheduling persistence in database

--- scheduling-api/src/main/java/com/arthur/schedulingApi/usecases/scheduling/CreateScheduling.java
+++ scheduling-api/src/main/java/com/arthur/schedulingApi/usecases/scheduling/CreateScheduling.java
@@ -3,20 +3,20 @@ package com.arthur.schedulingApi.usecases.scheduling;
 import com.arthur.schedulingApi.controllers.scheduling.request.SchedulingRequestDTO;
 import com.arthur.schedulingApi.controllers.scheduling.response.SchedulingResponseDTO;
 import com.arthur.schedulingApi.controllers.service.request.ServiceRequestDTO;
-import com.arthur.schedulingApi.exceptions.ServiceNotFoundException;
 import com.arthur.schedulingApi.models.scheduling.Scheduling;
 import com.arthur.schedulingApi.models.user.User;
+import com.arthur.schedulingApi.repositories.scheduling.SchedulingRepository;
 import com.arthur.schedulingApi.usecases.service.EditService;
 import com.arthur.schedulingApi.usecases.service.FindServiceById;
 import com.arthur.schedulingApi.usecases.user.FindUser;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 import java.util.Optional;
 
 import static com.arthur.schedulingApi.usecases.scheduling.mapper.SchedulingListToRequest.schedulingListToRequest;
 import static com.arthur.schedulingApi.usecases.scheduling.mapper.SchedulingToModel.schedulingToModel;
 import static com.arthur.schedulingApi.usecases.scheduling.mapper.SchedulingToResponse.schedulingToResponse;
+import static com.arthur.schedulingApi.usecases.service.mapper.ServiceToRequest.serviceToRequest;
 
 @Service
 public class CreateScheduling {
@@ -24,11 +24,13 @@ public class CreateScheduling {
     private final FindServiceById findServiceById;
     private final FindUser findUser;
     private final EditService editService;
+    private final SchedulingRepository schedulingRepository;
 
-    public CreateScheduling(FindServiceById findServiceById, FindUser findUser, EditService editService) {
+    public CreateScheduling(FindServiceById findServiceById, FindUser findUser, EditService editService, SchedulingRepository schedulingRepository) {
         this.findServiceById = findServiceById;
         this.findUser = findUser;
         this.editService = editService;
+        this.schedulingRepository = schedulingRepository;
     }
 
 
@@ -39,18 +41,13 @@ public class CreateScheduling {
         if (serviceOwner.isPresent()) {
 
             var service = findServiceById.findByIdAsModel(schedulingRequestDTO.serviceId());
+
             Scheduling scheduling = schedulingToModel(schedulingRequestDTO , service);
+            schedulingRepository.save(scheduling);
+
             service.addScheduling(scheduling);
 
-            editService.editService(service.getId() , new ServiceRequestDTO(
-                    service.getId() ,
-                    service.getName() ,
-                    service.getCapacity() ,
-                    service.getUrl_image(),
-                    service.getDescription(),
-                    service.getLocation(),
-                    schedulingListToRequest(service.getScheduling()))
-            );
+            editService.editService(service.getId() , serviceToRequest(service));
 
             return Optional.of(schedulingToResponse(scheduling));
         }
