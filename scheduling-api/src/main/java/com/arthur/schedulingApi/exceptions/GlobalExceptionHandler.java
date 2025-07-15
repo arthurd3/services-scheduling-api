deb964c89173c8720070b8203ef53dfd557c45b5
arthurd3 <arthur_camposl@yahoo.com>
2025-07-15 13:23:57 -0300

    feat: token jwt generation

--- scheduling-api/src/main/java/com/arthur/schedulingApi/exceptions/GlobalExceptionHandler.java
+++ scheduling-api/src/main/java/com/arthur/schedulingApi/exceptions/GlobalExceptionHandler.java
@@ -33,14 +33,14 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(EmailAlreadyExistsException.class)
     public final ErrorResponse handleResourceAlreadyExistsException(EmailAlreadyExistsException ex) {
-        var error = ErrorResponse.create(ex , HttpStatus.NOT_FOUND , ex.getMessage());
+        var error = ErrorResponse.create(ex , HttpStatus.CONFLICT , ex.getMessage());
         error.getBody().setTitle("E-mail ja utilizado");
         return error;
     }
 
     @ExceptionHandler(PhoneAlreadyExistsException.class)
     public final ErrorResponse handleResourceAlreadyExistsException(PhoneAlreadyExistsException ex) {
-        var error = ErrorResponse.create(ex , HttpStatus.NOT_FOUND , ex.getMessage());
+        var error = ErrorResponse.create(ex , HttpStatus.CONFLICT , ex.getMessage());
         error.getBody().setTitle("Telefone ja utilizado");
         return error;
     }
