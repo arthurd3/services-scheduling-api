dd7832835718acac132cadd954e105c7324bce4d
arthurd3 <arthur_camposl@yahoo.com>
2025-07-18 17:01:09 -0300

    feat: delete user by id test

---
+++ scheduling-api/src/test/java/com/arthur/schedulingApi/usecases/user/DeleteUserTest.java
@@ -0,0 +1,65 @@
+package com.arthur.schedulingApi.usecases.user;
+
+import com.arthur.schedulingApi.exceptions.UserNotFoundException;
+import com.arthur.schedulingApi.models.user.User;
+import com.arthur.schedulingApi.models.user.UserRoles;
+import com.arthur.schedulingApi.repositories.users.UserRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class DeleteUserTest {
+
+    @InjectMocks
+    private DeleteUser deleteUser;
+
+    @Mock
+    private UserRepository userRepository;
+
+
+    @Nested
+    class deleteById{
+
+        @Test
+        @DisplayName("Should delete user by ID with success")
+        void shouldDeleteById(){
+            //ARRANGE
+            var userDeleteId = 1L;
+
+            when(userRepository.existsById(userDeleteId)).thenReturn(true);
+
+            //ACTION
+            deleteUser.deleteUser(userDeleteId);
+
+            //ASSERTION
+
+            verify(userRepository, times(1)).deleteById(userDeleteId);
+        }
+
+        @Test
+        @DisplayName("Should Throw a UserNotFound excption")
+        void shouldThrowUserNotFoundOnDeleteById(){
+            //ARRANGE
+            var userDeleteId = 1L;
+
+            when(userRepository.existsById(userDeleteId)).thenReturn(false);
+
+            //ACTION
+            UserNotFoundException exception = assertThrows(UserNotFoundException.class,
+                    () -> deleteUser.deleteUser(userDeleteId));
+
+
+            //ASSERT
+            assertEquals("Nao foi possivel deletar o Usuario com id " + userDeleteId, exception.getMessage());
+        }
+
+    }
+}
