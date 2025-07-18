9f9fc29f09c333ce0b63690fd8ed62e9a975b3c9
arthurd3 <arthur_camposl@yahoo.com>
2025-07-18 14:06:25 -0300

    feat: should UserNotFoundException error

--- scheduling-api/src/test/java/com/arthur/schedulingApi/usecases/user/EditUserTest.java
+++ scheduling-api/src/test/java/com/arthur/schedulingApi/usecases/user/EditUserTest.java
@@ -1,8 +1,10 @@
 package com.arthur.schedulingApi.usecases.user;
 
 import com.arthur.schedulingApi.controllers.user.request.UserRequestDTO;
+import com.arthur.schedulingApi.exceptions.UserNotFoundException;
 import com.arthur.schedulingApi.models.user.User;
 import com.arthur.schedulingApi.models.user.UserRoles;
+import com.arthur.schedulingApi.repositories.users.UserRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -11,8 +13,10 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.mockito.Mockito.when;
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class EditUserTest {
@@ -22,6 +26,9 @@ class EditUserTest {
 
     @Mock
     private FindUser findUser;
+
+    @Mock
+    private UserRepository userRepository;
 
     @Nested
     class editUser{
@@ -54,6 +61,28 @@ class EditUserTest {
             assertEquals("arthur_camposl@yahoo.com" , editedUser.get().email() );
             assertEquals("321312321" , editedUser.get().phoneNumber() );
 
+        }
+
+
+        @Test
+        @DisplayName("Shoul UserNotFoundException erro")
+        void shouldUserNotFoundException() {
+
+            // ARRANGE
+            long userIdInexistente = 99L;
+            var userDTO = new UserRequestDTO("Nome Novo", "email@novo.com", "senha", "12345");
+
+
+            when(findUser.findUserEntity(userIdInexistente))
+                    .thenThrow(new UserNotFoundException("Usuario com id " + userIdInexistente + " nao encontrado!"));
+
+            // ACT
+            UserNotFoundException exception = assertThrows(UserNotFoundException.class, () -> {
+                editUser.editUser(userIdInexistente, userDTO);
+            });
+
+            //ASSERT
+            assertEquals("Usuario com id " + userIdInexistente + " nao encontrado!", exception.getMessage());
         }
     }
 }
