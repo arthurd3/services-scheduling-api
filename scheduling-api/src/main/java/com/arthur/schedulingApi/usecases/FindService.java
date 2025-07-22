a5a041601546fed98334091ac4bd3fc16b4253c8
arthurd3 <arthur_camposl@yahoo.com>
2025-07-22 15:24:57 -0300

    Add Redis on Docker-Compose

--- scheduling-api/src/main/java/com/arthur/schedulingApi/usecases/FindService.java
+++ scheduling-api/src/main/java/com/arthur/schedulingApi/usecases/FindService.java
@@ -5,6 +5,7 @@ import com.arthur.schedulingApi.exceptions.ServiceNotFoundException;
 import com.arthur.schedulingApi.models.Services;
 import com.arthur.schedulingApi.repositories.ServiceRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
@@ -12,13 +13,14 @@ import static com.arthur.schedulingApi.usecases.mapper.ServiceToResponse.service
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class FindService {
 
     private final ServiceRepository serviceRepository;
 
     @Cacheable(value = "SERVICE_CACHE", key = "#id")
     public ServiceResponseDTO findById(Long id){
-
+        log.info("### Buscando serviço com ID {} no BANCO DE DADOS... ###", id);
         var findService = serviceRepository.findById(id)
                 .orElseThrow(() -> new ServiceNotFoundException("Servico com id "+ id +" nao encontrado"));
         return serviceToResponse(findService);
